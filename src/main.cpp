a70559871c6e6e7cc2c192e7ebbda3eca8c91406
PlanatedLlama45 <supernotch17@gmail.com>
2024-03-23 21:19:45 +0300

    добавил функцию чтобы рандомизировать процент

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <Windows.h>
 
+int getRngPercentage() {
+    return rand() % 100;
+}
+
 int main() {
     srand(time(NULL));
     std::string name;
@@ -8,7 +12,7 @@ int main() {
     std::cin >> name;
     std::cout << "Calculating your sussyness...\n";
     Sleep(3000);
-    std::cout << "Yro'uertgk " << (rand() % 100) << "% sus :flushed: :skull:" << std::endl;
+    std::cout << "Yro'uertgk " << getRngPercentage() << "% sus :flushed: :skull:" << std::endl;
 
     return 0;
 }
